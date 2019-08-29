f14ea023517ec918ce72bc2a0bf828d4eb9525ff
roberto14118927 <robertoruiz141189@gmail.com>
2019-08-29 08:21:15 -0500

    Initial Commit

---
+++ Vantis/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Vantis
+//
+//  Created by Roberto Eduardo Guzman Ruiz on 8/29/19.
+//  Copyright © 2019 Roberto Eduardo Guzman Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ VantisTests/VantisTests.swift
@@ -0,0 +1,34 @@
+//
+//  VantisTests.swift
+//  VantisTests
+//
+//  Created by Roberto Eduardo Guzman Ruiz on 8/29/19.
+//  Copyright © 2019 Roberto Eduardo Guzman Ruiz. All rights reserved.
+//
+
+import XCTest
+@testable import Vantis
+
+class VantisTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
